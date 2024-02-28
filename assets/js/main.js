1eb335799fda7afa7a4f4dbe308507be7cd18336
JCionx <jcionx0@gmail.com>
2024-02-28 20:33:56 +0000

    Add z-index to topbar.css and implement installApp functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,7 +13,10 @@ if (typeof window.webkitConvertPointFromNodeToPage === 'function') {
 window.addEventListener('message', (event) => {
     console.log(event.data);
     let content = event.data;
-    if (content.action = "sendNotification") {
+    if (content.action == "sendNotification") {
         showNotification(content.appId, content.content, content.time);
+    }
+    if (content.action == "installApp") {
+        requestInstallApp(content.appId, content.name, content.icon, content.url);
     }
 })
